97f9ee56ae2a95893caf361f0c43e818c9d0d64c
pranee525 <praneeth5252@gmail.com>
2024-11-07 11:09:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-
+//some cahnge
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
